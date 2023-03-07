ca21199e2dc425c6bfff2f74e9b097fa58807032
themechanic002 <themechanic002@gmail.com>
2023-03-07 16:48:54 +0900

    Update: 코드 정렬

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,42 +1,66 @@
 enum GENDER {
-    MEN = 'MEN',
-    WOMEN = 'WOMEN',
-  }
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from 'typeorm';
+  MEN = 'MEN',
+  WOMEN = 'WOMEN',
+}
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('user')
-@Unique(['uesr_email'])     
+@Unique(['uesr_email'])
 export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Column({ name: 'user_id' })
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    @Column({name : 'user_id'})
-    id: string;
+  @Column({
+    name: 'user_email',
+    type: 'varchar',
+    length: 25,
+    comment: '유저 이메일',
+  })
+  email: string;
 
-    @Column({ name: 'user_email' , type: 'varchar', length: 25, comment: '유저 이메일' })
-    email: string;
-  
-    @Column({ name: 'user_password' ,type: 'varchar', length: 255, comment: '유저 비밀번호' })
-    password: string;
-  
-    @Column({ name: 'user_name' , type: 'varchar', length: 255, comment: '유저 이름' })
-    name: string;
-    @Column({ name : 'user_gender', type: 'enum', enum: GENDER, comment: '유저 성별(MEN, WOMEN)',})
-     gender: string;
+  @Column({
+    name: 'user_password',
+    type: 'varchar',
+    length: 255,
+    comment: '유저 비밀번호',
+  })
+  password: string;
 
-  
-    @Column({ name: 'user_genre' , type: 'varchar', length: 30, comment: '유저가 선호하는 장르' })
-    genre: string;
-  
-    @CreateDateColumn({ name: 'create_at', comment: '생성일' })
-    createdAt: Date;
-  
-    @UpdateDateColumn({ name: 'update_at', comment: '수정일' })
-    
-    updatedAt: Date;
-  
+  @Column({
+    name: 'user_name',
+    type: 'varchar',
+    length: 255,
+    comment: '유저 이름',
+  })
+  name: string;
+  @Column({
+    name: 'user_gender',
+    type: 'enum',
+    enum: GENDER,
+    comment: '유저 성별(MEN, WOMEN)',
+  })
+  gender: string;
 
+  @Column({
+    name: 'user_genre',
+    type: 'varchar',
+    length: 30,
+    comment: '유저가 선호하는 장르',
+  })
+  genre: string;
+
+  @CreateDateColumn({ name: 'create_at', comment: '생성일' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'update_at', comment: '수정일' })
+  updatedAt: Date;
 }
-
-
-
-
