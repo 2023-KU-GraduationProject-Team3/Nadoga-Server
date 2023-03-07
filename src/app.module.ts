dfb5a56ce6a4ae6c22edd78a811dcd5bdbe5d30a
themechanic002 <themechanic002@gmail.com>
2023-03-07 16:04:12 +0900

    Add: user and book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
+import { GenreModule } from './review/genre/genre.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BookModule, GenreModule],
   controllers: [AppController],
   providers: [AppService],
 })
