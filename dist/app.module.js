04a01265df2292fcf82d301ce88637cab55b798a
steve <steve4347@konkuk.ac.kr>
2023-03-20 14:55:46 +0900

    db 연동

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,11 +10,25 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const user_module_1 = require("./user/user.module");
+const book_module_1 = require("./book/book.module");
+const wishlist_module_1 = require("./wishlist/wishlist.module");
+const user_book_module_1 = require("./user_book/user_book.module");
+const review_module_1 = require("./review/review.module");
+const typeorm_1 = require("@nestjs/typeorm");
+const typeorm_configs_1 = require("./configs/typeorm.configs");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [
+            typeorm_1.TypeOrmModule.forRoot(typeorm_configs_1.typeORMConfig),
+            user_module_1.UserModule,
+            book_module_1.BookModule,
+            wishlist_module_1.WishlistModule,
+            user_book_module_1.UserBookModule,
+            review_module_1.ReviewModule,
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
