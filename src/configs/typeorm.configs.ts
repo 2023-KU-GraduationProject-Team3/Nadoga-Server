ddf14a68db6a5eb7c9440e56de896ad5ef6fb6fb
themechanic002 <themechanic002@gmail.com>
2023-03-21 13:57:11 +0900

    Add: config password

--- src/configs/typeorm.configs.ts
+++ src/configs/typeorm.configs.ts
@@ -1,12 +1,11 @@
-
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'min9211301',
+  password: 'glglgl',
   database: 'nadoga',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
