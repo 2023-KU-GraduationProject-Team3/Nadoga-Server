379dc81896dc254491ce4dd5a567b5767c1bff9e
steve <steve4347@konkuk.ac.kr>
2023-03-21 13:59:26 +0900

    Add: password

--- src/configs/typeorm.configs.ts
+++ src/configs/typeorm.configs.ts
@@ -5,7 +5,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'glglgl',
+  password: 'min9211301',
   database: 'nadoga',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
