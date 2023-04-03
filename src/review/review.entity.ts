a602a82f099cce3329c10db89560fd0991bc0a7c
steve <steve4347@konkuk.ac.kr>
2023-04-03 14:54:38 +0900

    Add : 유저가 찜한 도서 추가 컨트롤러

--- src/review/review.entity.ts
+++ src/review/review.entity.ts
@@ -10,7 +10,6 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
-//도서는 여러가지의 리뷰를 가질수 있고 리뷰는 하나의 도서만 가질수 있음 ManyToOne이 적절
 @Entity('review')
 export class Review extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
