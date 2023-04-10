f1a6ed9178aa092b60c81a0c39d74e343caa53ff
singjun <shinsj4653@gmail.com>
2023-04-10 22:43:53 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,8 +19,13 @@ export class UserController {
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     const emailExists = await this.userService.emailExists(createUserDto.email);
 
+<<<<<<< Updated upstream
     if (emailExists !== undefined) {
       throw new ConflictException('이미 존재하는 이메일입니다.');
+=======
+    if (emailExists == undefined) {
+      throw new ConflictException('이미 존재하는 이메일 입니다.');
+>>>>>>> Stashed changes
     }
 
     return this.userService.createUser(createUserDto);
