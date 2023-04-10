34238164da03898876069566dec9ee1bfb71cf33
themechanic002 <themechanic002@gmail.com>
2023-04-10 22:37:54 +0900

    Fix: ==undefined

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,8 +19,8 @@ export class UserController {
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     const emailExists = await this.userService.emailExists(createUserDto.email);
 
-    if (emailExists != undefined) {
-      throw new ConflictException('이미 존재하는 이메일 입니다.');
+    if (emailExists !== undefined) {
+      throw new ConflictException('이미 존재하는 이메일입니다.');
     }
 
     return this.userService.createUser(createUserDto);
