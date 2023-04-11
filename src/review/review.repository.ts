1eb205e70775889f3d231e6a9e605fc6a036481e
steve <steve4347@konkuk.ac.kr>
2023-04-11 17:34:29 +0900

    fix : console.log

--- src/review/review.repository.ts
+++ src/review/review.repository.ts
@@ -5,11 +5,15 @@ import { Review } from './review.entity';
 @CustomRepository(Review)
 export class ReviewRepository extends Repository<Review> {
   async getCollab(): Promise<any> {
-    return this
+    const result = await this
       .query(`SELECT r.review_rating , r.book_isbn , r.user_id , u.user_genre , u.user_age , u.user_gender
       FROM review r
       INNER JOIN user u ON r.user_id  = u.id 
       ORDER BY r.create_at  DESC
       LIMIT 10;`);
+  
+    console.log(result); // 결과값 출력
+  
+    return result;
   }
 }
