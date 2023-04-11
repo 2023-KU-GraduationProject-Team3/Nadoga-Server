3671f4122549685bb8b5aa1bd2f077c9d9852a7a
themechanic002 <themechanic002@gmail.com>
2023-04-11 17:08:31 +0900

    Build

--- src/review/review.repository.ts
+++ src/review/review.repository.ts
@@ -5,7 +5,8 @@ import { Review } from './review.entity';
 @CustomRepository(Review)
 export class ReviewRepository extends Repository<Review> {
   async getCollab(): Promise<any> {
-    return this.query(`SELECT r.rating, r.book_isbn, r.user_id, u.genre, u.age, u.gender, r.createdAt
+    return this
+      .query(`SELECT r.rating, r.book_isbn, r.user_id, u.genre, u.age, u.gender, r.createdAt
     FROM review r
     INNER JOIN user u ON r.user_id = u.id
     WHERE r.createdAt IN (
