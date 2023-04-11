a681fa2235794b4e54e12e08d419d89c66653035
steve <steve4347@konkuk.ac.kr>
2023-04-11 17:26:44 +0900

    Add: queryUpdate

--- src/review/review.repository.ts
+++ src/review/review.repository.ts
@@ -6,16 +6,10 @@ import { Review } from './review.entity';
 export class ReviewRepository extends Repository<Review> {
   async getCollab(): Promise<any> {
     return this
-      .query(`SELECT r.rating, r.book_isbn, r.user_id, u.genre, u.age, u.gender, r.createdAt
-    FROM review r
-    INNER JOIN user u ON r.user_id = u.id
-    WHERE r.createdAt IN (
-      SELECT r2.createdAt
-      FROM review r2
-      WHERE r2.user_id = r.user_id
-      ORDER BY r2.createdAt DESC
-      LIMIT 10
-    )
-    ORDER BY u.id, r.createdAt DESC;`);
+      .query(`SELECT r.review_rating , r.book_isbn , r.user_id , u.user_genre , u.user_age , u.user_gender
+      FROM review r
+      INNER JOIN user u ON r.user_id  = u.id 
+      ORDER BY r.create_at  DESC
+      LIMIT 10;`);
   }
 }
